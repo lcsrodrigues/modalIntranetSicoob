10776414796545278485b43f029c6a4b81362efe
Lucas Rodrigues <lcs-rodrigues@hotmail.com>
2022-04-29 21:47:39 -0300

    ajustes de add imagem, cor e link

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -5,29 +5,38 @@ import axios from 'axios';
 
 export default function Modal() {
 
-    var isVisibleModal = window.localStorage.getItem("isVisibleModal");
+    // var isVisibleModal = window.localStorage.getItem("isVisibleModal");
     const [isVisible, setIsVisible] = useState(true);//useState(isVisibleModal == null ? 'true' : isVisibleModal);
-    const [modalDescription, setModalDescription] = useState("");
     const [modalTitle, setModalTitle] = useState("");
+    const [urlImage, setUrlImage] = useState("");
+    const [linkImage, setLinkImage] = useState("");
+    const [backgroundColor, setBackGroundColor] = useState("#00A288");
 
     useEffect(() => {
         function getInfoForModal() {
             axios.get("https://crediminas.sharepoint.com/sites/intranet/_api/web/lists/getbytitle('Informações Modal')/items?$top=1&$orderby=ID desc")
-                .then(function (result) {
-                    console.log(result);
+                .then((result) => {
                     setModalTitle(result.data.value[0].Title);
-                    setModalDescription(result.data.value[0].DescricaoModal);
+                    setUrlImage(result.data.value[0].urlImagem);
+                    setLinkImage(result.data.value[0].linkImagem);
+                    setBackGroundColor(result.data.value[0].backgroundColor);
                 })
-                .catch(function (err) {
+                .catch((err) => {
                     console.log(err);
                 })
         }
         function visibibleModal() {
-            window.localStorage.setItem("isVisibleModal", isVisible);
+            // window.localStorage.setItem("isVisibleModal", isVisible);
+            if (window.location.href === "https://crediminas.sharepoint.com/sites/intranet?debug=true&noredir=true&debugManifestsFile=https://localhost:4321/temp/manifests.js") {
+
+                setIsVisible(true);
+            } else {
+                setIsVisible(false);
+            }
         }
         getInfoForModal();
-        //visibibleModal();
-    }, [isVisible])
+        visibibleModal();
+    }, [])
 
     function closeModal() {
         //setIsVisible('false');
@@ -45,9 +54,12 @@ export default function Modal() {
                         <img src={require('../../images/close.png')} />
                     </div>
                 </div>
-                <div className={styles.content}>
+                <div className={styles.content} style={{ backgroundColor: backgroundColor }}>
                     <div className={styles.sideLeft}>
-                        <p dangerouslySetInnerHTML={{ __html: modalDescription }} />
+                        <a href={linkImage != null ? linkImage : ''} target={linkImage != null ? "_blank" : ''}>
+                            <img src={urlImage} />
+                        </a>
+                        {/* <p dangerouslySetInnerHTML={{ __html: modalDescription }} /> */}
                     </div>
                     {/* <div className={styles.sideRight}>
                         <p>{modalDescription}</p>
